2d1c23560c8e09cf23ef7907520eee0ac4a7bd33
Michael Rodriguez <michael.rodriguez@n2ia-technologies.com>
2026-06-12 12:20:29 -0400

    I liked the edit of raw

--- script.js
+++ script.js
@@ -232,7 +232,7 @@ const CAPTCHA_POOL = [
     caption: "I'm not afraid of a little blood either 😈" },
   { src: "Cherry.jpg", label: "Cherry", gif: "Cherry.gif",
     caption: "Te estoy velando graciosa 👀, at least she's not a friend lol." },
-  { src: "RauwAlejandro.jpg", label: "RauwAlejandro", worm: "Gusano.gif",
+  { src: "RauwAlejandro.jpg", label: "RauwAlejandro", worm: true,
     caption: "De verdad quieres que vaya a tu casa a hacerte el gusano ese?" },
   { src: "TaylorSwift.jpg", label: "TaylorSwift",
     caption: "Do you wanna get swifted? 💀" },
